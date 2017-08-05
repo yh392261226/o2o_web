e842733108e645f954d1ad874100b946ba02732a
杨浩 <yh392261226@gmail.com>
2017-08-05 14:54:23 +0800

    new begin

--- apps/manage/index.php
+++ apps/manage/index.php
@@ -3,3 +3,5 @@ define('WEBPATH', dirname(__FILE__));
 require WEBPATH . '/configs/config.php';
 require FRAMEWORKPATH . '/libs/lib_config.php';
 Swoole::$php->setAppPath(APPPATH);
+//Swoole::$php->runMVC();
+Swoole::$php->runConsole();
