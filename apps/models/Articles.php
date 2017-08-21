5c410e44ab9556c5c069dd46e393db6b90506283
空格不在 <yh392261226@sina.com>
2017-08-21 08:54:15 +0800

    Revert "文章分类修改删除功能"

--- apps/models/Articles.php
+++ apps/models/Articles.php
@@ -3,12 +3,12 @@
  * @Author: Zhaoyu
  * @Date:   2017-08-14 16:07:50
  * @Last Modified by:   Zhaoyu
- * @Last Modified time: 2017-08-18 16:22:22
+ * @Last Modified time: 2017-08-14 16:09:33
  */
 namespace App\Model;
 
 
-class Articles extends \CLASSES\ModelBase
+class Managers extends \CLASSES\ModelBase
 {
     /*文章表*/
     public $table   = 'articles';
