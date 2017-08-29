945254a813fc43263b7eb062aaba4f533a21d574
zhaoyu <zhaoyu8292@qq.com>
2017-08-29 14:25:44 +0800

    清空master

--- apps/manage/configs/upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$upload = array(
-    'base_dir' => WEBPATH.'/../uploads/images',
-    'base_url' => "/",
-);
-// var_dump($upload);die;
-return $upload;
--- apps/web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('WEBPATH', dirname(__FILE__));
-require WEBPATH . '/configs/config.php';
-require FRAMEWORKPATH . '/libs/lib_config.php';
-Swoole::$php->setAppPath(APPPATH);
-//Swoole::$php->runMVC();
-Swoole::$php->runConsole();
