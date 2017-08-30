ea7e126c5cceb16150b90c225fc51871ab8a8e0d
zhaoyu <zhaoyu8292@qq.com>
2017-08-30 16:27:33 +0800

    文章部分修改

---
+++ apps/manage/controllers/dome.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @Author: Zhaoyu
+ * @Date:   2017-08-29 16:21:05
+ * @Last Modified by:   Zhaoyu
+ * @Last Modified time: 2017-08-29 16:21:17
+ */
+namespace App\Controller;
+class Index extends \CLASSES\ManageBase
+{
+    public function __construct($swoole)
+    {
+        parent::__construct($swoole);
+        $this->managers_dao = new \MDAO\Managers();
+    }
+    public function test()
+    {
+        if (DEBUG)
+        {
+            $this->db->debug = 1;
+            //$this->showTrace(1);
+            //$this->showTime();
+        }
+        //Login:
+        //$data = array(
+        //    'm_name' => 'test',
+        //    'm_pass' => encyptPassword('test'),
+        //);
+        //$login  = $this->managers_dao->infoData(array('key' => 'm_name', 'val' =>  $data['m_name']));
+        //if (!empty($login) && $login['m_pass'] == $data['m_pass'])
+        //{
+        //    $_SESSION['manager'] = $login;
+        //}
+        //print_r($_SESSION);
+        //------------------------------------------------------------------------------------------------
+        //List:
+        //$data   = array('pager' => true, 'page' => 1, 'm_status' => -2, 'm_id' => array('type' => 'in', 'value' => '1,2,3'));
+        //$data   = array('pager' => true, 'page' => 1, 'm_status' => -2, 'm_id' => array('type' => 'in', 'value' => array(1,2,3,4)));
+        $data   = array('pager' => true, 'page' => 1, 'm_status' => -2, 'm_name' => array('type' => 'like', 'value' => 'admin'), 'm_id' => array('type' => 'in', 'value' => array(1,2,3,4)), 'm_in_time' => array(array('type' => 'ge', 'ge_value' => 1), array('type' => 'le', 'le_value' => 9999999999)));//多个条件并存
+        //$data   = array('pager' => true, 'page' => 1, 'm_status' => -2, 'm_in_time' => array(array('type' => 'ge', 'ge_value' => 1), array('type' => 'le', 'le_value' => 9999999999)));//2个区间值
+        //$data   = array('pager' => true, 'page' => 1, 'm_status' => -2, 'm_in_time' => array('type' => 'ge', 'ge_value' => 2)); //单个区间值
+        $list = $this->managers_dao->listData($data);
+        print_r($list);
+        //------------------------------------------------------------------------------------------------
+        //Info:
+        //$info = $this->managers_dao->infoData('1');
+        //$info = $this->managers_dao->infoData(array('key' => 'm_name', 'val' =>  $data['m_name']));
+        //print_r($info);
+        //------------------------------------------------------------------------------------------------
+        //Count:
+        //$data   = array('m_status' => -2);
+        //$counts = $this->managers_dao->countData($data);
+        //print_r($counts);
+        //------------------------------------------------------------------------------------------------
+        //Add:
+        //$data = array(
+        //    'm_name' => 'test2',
+        //    'm_pass' => encyptPassword('test2'),
+        //    'm_status' => 0,
+        //    'm_in_time' => time(),
+        //    'm_inip' => getIp(),
+        //    'm_author' => 0,
+        //    'mpg_id' => 0,
+        //    'm_start_time' => time(),
+        //    'm_end_time' => time(),
+        //    'm_last_edit_time' => time(),
+        //    'm_last_editor' => 0,
+        //    'm_last_ip' => getIp(),
+        //    );
+        //$add  = $this->managers_dao->addData($data);
+        //var_dump($add);
+        //------------------------------------------------------------------------------------------------
+        //Delete:
+        //$delete = $this->managers_dao->delData(array('m_id' => array('type' => 'in', 'value' => array(1,2,3)))); //伪删除
+        //$delete = $this->managers_dao->delData('7'); //伪删除
+        //$delete = $this->managers_dao->delData(array('m_id' => array('type' => 'in', 'value' => array(8,9,20)))); //真删除
+        //var_dump($delete);
+        //------------------------------------------------------------------------------------------------
+        //Update:
+        //$data = array(
+        //    'm_name' => 'test2',
+        //    'm_pass' => encyptPassword('test2'),
+        //    'm_status' => 0,
+        //    'm_author' => 1,
+        //    'mpg_id' => 2,
+        //    'm_last_edit_time' => time(),
+        //    'm_last_editor' => 2,
+        //    'm_last_ip' => getIp(),
+        //    );
+        //$param = array('m_id' => 7);
+        //$update  = $this->managers_dao->updateData($data, $param);
+        //var_dump($update);
+        //------------------------------------------------------------------------------------------------
+        //Sql:
+        //$sql = 'show tables';
+        //$query = $this->managers_dao->queryData($sql);
+        //print_r($query->fetchall());
+    }
+}
--- apps/manage/dao/Articles.php
+++ apps/manage/dao/Articles.php
@@ -3,8 +3,110 @@ namespace MDAO;
 
 class Articles extends \MDAOBASE\DaoBase
 {
-    public function __construct()
+	public $articles     = null;
+	public $articles_ext = null;
+
+	public function __construct($data)
     {
-        parent::__construct(array('table' => 'Managers'));
+    	parent::__construct($data);
+	}
+
+	/**
+     * 获取文章分类数组
+     * @author zhaoyu
+     * @e-mail zhaoyu8292@qq.com
+     * @date   2017-08-14
+     * @return [type]            [description]
+     */
+
+    /*获取所有除了自己子集的分类树*/
+    public function getTreeExceptChild($catid)
+    {
+        $data = $this->listData();
+        $child_arr = _getChildren($data,$catid,true);
+
+        if($child_arr){
+            $ins = implode(',', $child_arr);
+            $no_child_arr = $this->listData(array('fields'=>'ac_id,ac_name,ac_pid',"where"=>"`ac_id` NOT IN ({$ins})"));
+        }else{
+            $no_child_arr = $data;
+        }
+
+        return $this->_getTree($no_child_arr,0,0,true);
+
     }
+     /*递归引用*/
+    public function getCategotyChildren($catid)
+    {
+        $m_ac = model('ArticlesCategory');
+        $data = $this->listData();
+        return _getChildren($data,$catid,true);
+
+    }
+
+    /*递归引用输出前台树状模型(带level)*/
+    public function getTree($catid=0){
+        $data = $this->listData(array('fields'=>'ac_id,ac_name,ac_pid','where'=>1));
+        $data = $data['data'];
+        return $this->_getTree($data,$catid,0,true);
+    }
+
+    /*递归引用输出前台树状模型(带level)*/
+    public function _getTree($data,$pid,$level=0,$isClear=false){
+        static $tree = array();
+        if($isClear){
+            $tree = array();
+        }
+        foreach ($data as $k => $v) {
+            if($pid == $v['ac_pid']){
+                $v['level'] = $level;
+                $tree[] = $v;
+                $this->_getTree($data,$v['ac_id'],$level+1);
+            }
+        }
+        return $tree;
+    }
+
+    /*递归引用输出树状模型*/
+    public function getChildTree($catid=0){
+
+        $data = $this->listData(array('fields'=>'ac_id as tags ,ac_pid,ac_name as text','where'=>1));
+
+        return $this->_getChildTree($data['data'],$catid);
+    }
+    /*递归引用输出树状模型*/
+    public function _getChildTree($data,$pid){
+
+        $tree = array();
+
+        foreach ($data as $k => $v) {
+            if($pid == $v['ac_pid']){
+                $res = $this->_getChildTree($data,$v['tags']);
+                if($res){
+                   $v['nodes'] =  $res;
+                }
+                unset($v['ac_pid']);
+                $tree[] = $v;
+            }
+
+        }
+        return $tree;
+    }
+
+	/*递归处理 输出所有子分类id*/
+    public function _getChildren($data,$catid,$isClear=false)
+    {
+        static $child = array();
+        if($isClear){
+            $child = array();
+        }
+        foreach ($data as $k => $v) {
+            if($v['ac_pid'] == $catid){
+                $child[] = $v['ac_id'];
+                $this->_getChildren($data,$v['ac_id']);
+            }
+        }
+        return $child;
+    }
+
 }
