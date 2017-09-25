f5c5df683c8b7f93d558c2a2ec5ff5b58d79ccff
zhaoyu <zhaoyu8292@qq.com>
2017-09-25 17:10:33 +0800

    用户头像上传,通过技能id返回工人列表功能修改

--- apps/web/configs/config.php
+++ apps/web/configs/config.php
@@ -23,5 +23,7 @@ define('PAGESIZE', 25); //默认每页显示条数
 define('HTMLEDITOR', '../classes/kindeditor'); //定义富文本编辑器目录
 define('UPLOADURL', '/'); //定义上传url路径
 define('UPLOADPATH', WEBPATH.'/../uploads'); //定义上传路径地址
+define('PI',3.1415926535898);
+define('EARTH_RADIUS',6378.137);
 // kindeditor文件上传路径必须在\o2o_web\apps\manage\classes\kindeditor\php\JSON.php下修改
 //define('CACHEDB', ''); //数据库的缓存文件目录
