bc7d827fb9b38c119346882a23ce10145c7c5613
杨浩 <yh392261226@gmail.com>
2017-10-12 15:08:03 +0800

    developing api:Tasks

--- apps/web/controllers/Tasks.php
+++ apps/web/controllers/Tasks.php
@@ -72,6 +72,8 @@ class Tasks extends \CLASSES\WebBase
         if (isset($_REQUEST['t_author'])) $data['t_author'] = intval($_REQUEST['t_author']);
         if (isset($_REQUEST['t_phone'])) $data['t_phone'] = intval($_REQUEST['t_phone']);
         if (isset($_REQUEST['t_phone_status'])) $data['t_phone_status'] = intval($_REQUEST['t_phone_status']);
+        $data['t_storage'] = 0;
+        if (isset($_REQUEST['t_storage'])) $data['t_storage'] = intval($_REQUEST['t_storage']);
 
         //price between
         if (isset($_REQUEST['ge_amount']) && floatval($_REQUEST['ge_amount']) > 0) $data['t_amount'][0] = array('type' => 'ge', 'ge_value' => floatval($_REQUEST['ge_amount']));
@@ -95,7 +97,7 @@ class Tasks extends \CLASSES\WebBase
         if (isset($_REQUEST['skills']) && intval($_REQUEST['skills']) > 0) $data['leftjoin'] = array('task_ext_worker', ' task_ext_worker.t_id = tasks.t_id and task_ext_worker.tew_skills = "' . intval($_REQUEST['skills']) . '"');
         if (isset($data['leftjoin']))
         {
-            $data['fields'] = 'tasks.t_id, tasks.t_title, tasks.t_status, tasks.t_author, tasks.t_phone, tasks.t_phone_status, tasks.t_amount, tasks.t_edit_amount, tasks.t_duration, tasks.t_amount_edit_times, tasks.t_posit_x, tasks.t_posit_y, tasks.t_in_time, task_ext_worker.tew_skills, task_ext_worker.tew_worker_num, task_ext_worker.tew_price, task_ext_worker.tew_start_time, task_ext_worker.tew_end_time, task_ext_worker.r_province, task_ext_worker.r_city, task_ext_worker.r_area, task_ext_worker.tew_address, task_ext_worker.tew_lock';
+            $data['fields'] = 'tasks.t_id, tasks.t_title, tasks.t_status, tasks.t_author, tasks.t_phone, tasks.t_phone_status, tasks.t_amount, tasks.t_edit_amount, tasks.t_duration, tasks.t_amount_edit_times, tasks.t_posit_x, tasks.t_posit_y, tasks.t_in_time, tasks.t_storage, task_ext_worker.tew_skills, task_ext_worker.tew_worker_num, task_ext_worker.tew_price, task_ext_worker.tew_start_time, task_ext_worker.tew_end_time, task_ext_worker.r_province, task_ext_worker.r_city, task_ext_worker.r_area, task_ext_worker.tew_address, task_ext_worker.tew_lock';
         }
         $data['pager'] = 0;
         $data['order'] = 't_id desc';
@@ -175,6 +177,70 @@ class Tasks extends \CLASSES\WebBase
         {
             $this->exportData();
         }
+    }
+
+    /**
+     * 任务添加
+     */
+    public function add()
+    {
+        $data = $message = $tmp = array();
+        if (isset($_REQUEST['basic'][0]['data']) && '' != trim($_REQUEST['basic'][0]['data'])) $data['t_title'] = trim($_REQUEST['basic'][0]['data']);
+        if (!isset($data['t_title'])) $message[] = '标题不能为空';
+        if (isset($_REQUEST['basic'][1]['data']) && '' != trim($_REQUEST['basic'][1]['data'])) $data['t_info'] = $data['t_desc'] = trim($_REQUEST['basic'][1]['data']);
+        if (!isset($data['t_info'])) $message[] = '简介不能为空';
+        if (isset($_REQUEST['t_amount']) && 0 < floatval($_REQUEST['t_amount'])) $data['t_amount'] = $data['t_edit_amount'] = floatval($_REQUEST['t_amount']);
+        if (!isset($data['t_amount'])) $message[] = '任务总价不能小于等于0';
+        if (isset($_REQUEST['t_duration']) && 1 <= intval($_REQUEST['t_duration'])) $data['t_duration'] = intval($_REQUEST['t_duration']);
+        if (!isset($data['t_duration'])) $message[] = '任务时长不能小于1天';
+        if (isset($_REQUEST['t_posit_x']) && is_float($_REQUEST['t_posit_x'])) $data['t_posit_x'] = floatval($_REQUEST['t_posit_x']);
+        if (!isset($data['t_posit_x'])) $message[] = 'x轴坐标不正确';
+        if (isset($_REQUEST['t_posit_y']) && is_float($_REQUEST['t_posit_y'])) $data['t_posit_y'] = floatval($_REQUEST['t_posit_y']);
+        if (!isset($data['t_posit_y'])) $message[] = 'y轴坐标不正确';
+        if (isset($_REQUEST['t_author']) && 0 < intval($_REQUEST['t_author'])) $data['t_author'] = $data['t_last_editor'] = intval($_REQUEST['t_author']);
+        if (!isset($data['t_author'])) $message[] = '发布人id不正确';
+        if (isset($_REQUEST['t_phone']) && '' != trim($_REQUEST['t_phone'])) $data['t_phone'] = trim($_REQUEST['t_phone']);
+        if (isset($_REQUEST['t_type']) && '' != trim($_REQUEST['t_type'])) $data['t_type'] = trim($_REQUEST['t_type']);
+        $data['t_phone_status'] = 1;
+        if (isset($_REQUEST['t_phone_status']) && in_array($_REQUEST['t_phone_status'], array(0, 1))) $data['t_phone_status'] = intval($_REQUEST['t_phone_status']);
+        $data['t_in_time'] = $data['t_last_edit_time'] = time();
+        $data['t_amount_edit_times'] = 0;
+        $data['t_status'] = 0;
+        if (isset($_REQUEST['t_status']) && is_int($_REQUEST['t_status'])) $data['t_status'] = intval($_REQUEST['t_status']);
+        $data['t_storage'] = 0;
+        if (isset($_REQUEST['t_storage']) && is_int($_REQUEST['t_storage'])) $data['t_storage'] = intval($_REQUEST['t_storage']);
+
+        if (isset($_REQUEST['province']) && intval($_REQUEST['province']) > 0) $tmp['province'] = intval($_REQUEST['province']);
+        if (isset($_REQUEST['city']) && intval($_REQUEST['city']) > 0) $tmp['city'] = intval($_REQUEST['city']);
+        if (isset($_REQUEST['area']) && intval($_REQUEST['area']) > 0) $tmp['area'] = intval($_REQUEST['area']);
+        if (isset($_REQUEST['address']) && '' != trim($_REQUEST['address'])) $tmp['address'] = trim($_REQUEST['address']);
+
+        /*tasks_ext_info*/
+        if (isset($_REQUEST['t_desc']) && '' != trim($_REQUEST['t_desc'])) $data['t_desc'] = trim($_REQUEST['t_desc']);
+
+        /*tasks_ext_worker*/
+        if (isset($_REQUEST['worker']) && !empty($_REQUEST['worker']))
+        {
+            if (!empty($_REQUEST['worker']))
+            {
+                $worker = array();
+                foreach ($_REQUEST['worker'] as $key => $val)
+                {
+                    $worker[$key]['tew_skills'] = $val['skill'];
+                    $worker[$key]['tew_worker_num'] = $val['personNum'];
+                    $worker[$key]['tew_price'] = $val['money'];
+                    $worker[$key]['tew_start_time'] = $val['startTime'];
+                    $worker[$key]['tew_end_time'] = $val['endTime'];
+                    $worker[$key]['r_province'] = isset($tmp['province']) ? $tmp['province'] : 0;
+                    $worker[$key]['r_city'] = isset($tmp['city']) ? $tmp['city'] : 0;
+                    $worker[$key]['r_area'] = isset($tmp['area']) ? $tmp['area'] : 0;
+                    $worker[$key]['tew_address'] = isset($tmp['address']) ? $tmp['address'] : '';
+                    $worker[$key]['tew_lock'] = 0;
+                }
+            }
+        }
+
+
     }
 
 }
