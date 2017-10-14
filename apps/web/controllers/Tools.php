1e45a278fb15c87e67f653b18095d2360afe01cc
杨浩 <yh392261226@gmail.com>
2017-10-14 16:57:54 +0800

    modify: web:tools

--- apps/web/controllers/Tools.php
+++ apps/web/controllers/Tools.php
@@ -21,4 +21,22 @@ class Tools extends \CLASSES\WebBase
             'his'       => date('H:i:s'), //22:10:10
         );
     }
+
+    public function subTotal()
+    {
+        $result = 0;
+        if (!empty($_REQUEST['worker']))
+        {
+            $worker = array();
+            foreach ($_REQUEST['worker'] as $key => $val)
+            {
+                $worker[$key][0] = isset($val[1]['personNum']) ? intval($val[1]['personNum']) : 0;
+                $worker[$key][1] = isset($val[1]['money']) ? floatval($val[1]['money']) : 0;
+                $worker[$key][2] = isset($val[2]['startTime']) ? strtotime($val[2]['startTime']) : 0;
+                $worker[$key][3] = isset($val[2]['endTime']) ? strtotime($val[2]['endTime']) : 0;
+                $result += $worker[$key][0] * $worker[$key][1] * (ceil($worker[$key][3] - $worker[$key][2]) / 3600 / 24 + 1);
+            }
+        }
+        return $result;
+    }
 }
