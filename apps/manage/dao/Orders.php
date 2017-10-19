c2fd7e9bba14be72cc6f05d82261dd9aeeb3df58
杨浩 <yh392261226@gmail.com>
2017-10-19 17:40:29 +0800

    developing api&manager:Orders

--- apps/manage/dao/Orders.php
+++ apps/manage/dao/Orders.php
@@ -7,4 +7,20 @@ class Orders extends \MDAOBASE\DaoBase
     {
         parent::__construct(array('table' => 'Orders'));
     }
+
+    public function updateStatus($data = array(), $param = array())
+    {
+        if (!empty($data) && !empty($param))
+        {
+            $result = $this->updateData($data, $param);
+            if (isset($data['tewo_status']) && in_array($data['tewo_status'], array(-3, 2)))
+            {
+                if ($data['tewo_status'] == -3) $status = 2;
+                if ($data['tewo_status'] == 2) $status = 3;
+                model('Task_ext_worker_order')->updateData(array('tewo_status' => $status), array('o_id' => $param['o_id']));
+            }
+            return $result;
+        }
+        return false;
+    }
 }
