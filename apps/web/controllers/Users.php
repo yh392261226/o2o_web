9c1b94c1c366b41bcb2564fe911232e3d6ad4317
yjxsf <37399736+yjxsf@users.noreply.github.com>
2018-04-14 10:53:41 +0800

    修改用户
    修改用户信息 身份证选填

--- apps/web/controllers/Users.php
+++ apps/web/controllers/Users.php
@@ -208,7 +208,7 @@ class Users extends \CLASSES\WebBase
             unset($_REQUEST);
             $_REQUEST = $data_r;
 
-        }elseif (empty($_REQUEST['u_id']) || !isset($_REQUEST['u_sex']) || empty($_REQUEST['u_true_name']) || empty($_REQUEST['u_idcard']) || empty($_REQUEST['uei_info']) || empty($_REQUEST['uei_address']) || empty($_REQUEST['uei_province']) || empty($_REQUEST['uei_city']) || empty($_REQUEST['uei_area'])){
+        elseif (empty($_REQUEST['u_id'])  || empty($_REQUEST['u_true_name']) || empty($_REQUEST['uei_province']) || empty($_REQUEST['uei_city']) || empty($_REQUEST['uei_area'])){
              $this->exportData( array('msg'=>'参数不足'),0);
         }
 
